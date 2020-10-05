846200e49fd08113c820eae6b499adad9b84f808
nadeem-uiux <nadeem@thezudo.com>
2020-10-05 16:38:12 +0530

    Changed modal action position

--- src/views/ModalExample.vue
+++ src/views/ModalExample.vue
@@ -11,9 +11,9 @@
           <div class="max-w-lg w-full bg-white rounded-lg shadow-2xl px-6 py-6">
             <h2 class="font-semibold text-gray-900 text-2xl leading-tight border-b-2 border-gray-200 pb-4">Delete account</h2>
             <p class="mt-4 text-gray-700">Are you absolutely sure you want to cancel your subscription and delete your account? This action cannot be undone.</p>
-            <div class="mt-6">
+            <div class="mt-6 text-right">
               <button type="button" @click="open = false" class="px-4 py-2 text-white font-semibold bg-red-500 hover:bg-red-600 border border-transparent rounded focus:outline-none focus:shadow-outline">
-                Delete my account
+                Sure, Delete
               </button>
               <button @click="open = false" class="ml-4 px-4 py-2 text-gray-800 font-semibold bg-white hover:bg-gray-100 border rounded focus:outline-none focus:shadow-outline">
                 Cancel
